00fe8c185778d66613889a2bd9b8f0f342434ee8
Avesh Verma <verma.avesh001@gmail.com>
2024-04-03 13:46:12 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Avesh Verma",
+    author_email="verma.avesh001@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
