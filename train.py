1be46083b2113c818013147b9e46c3f50a34ee3f
Avesh Verma <verma.avesh001@gmail.com>
2024-04-05 17:58:37 +0530

    added data ingestion and data transformation

--- train.py
+++ train.py
@@ -1,5 +1,17 @@
-from ner.configuration.gcloud import GCloud
+import os
+import sys
+from ner.exception import NerException
+from ner.pipeline.train_pipeline import TrainPipeline
+from ner.constants import *
+def training():
+    try:
+        train_pipeline = TrainPipeline()
 
-obj  = GCloud()
+        train_pipeline.run_pipeline()
 
-obj.sync_folder_from_gcloud(gcp_bucket_url ="ner-using-bert-avesh", filename="archive.zip", destination="data/archive.zip")
+    except Exception as e:
+        raise NerException(e, sys) from e
+
+
+if __name__ == "__main__":
+    training()
